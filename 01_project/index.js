56007b2ab2aa687599a504286cb0a589dee29897
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-10-18 00:28:04 +0530

    mongoDB tutorial

--- 01_project/index.js
+++ 01_project/index.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
 const app = express();
+const mongoose = require("mongoose")
 
 const port = 8000;
+
+//connection
+mongoose.connect("mongodb://127.0.0.1:27017/test-database1").then(()=>{
+  console.log("mongo connected");
+}).catch((err) =>{
+  console.log(err);
+})
+
+//schema
+const userSchema = new mongoose.Schema({
+  first_name:{
+    type: String,
+    required: true
+  },
+  last_name:{
+    type: String,
+  },
+  email:{
+    type:String,
+    required: true,
+    unique: true
+  },
+  job_title:{
+    type: String
+  },
+  gender:{
+    type: String
+  },
+})
+//model
+const User = mongoose.model('user',userSchema)
+
+//creating entry
+User.create({
+  first_name:"Siddharth",
+  last_name : "Dhodi",
+  email: "siddharth.dhodi.work@gmail.com",
+  job_title: "software-developer",
+  gender:"Male"
+})
 
 app.get('/users',(req,res) =>{
   const html =
